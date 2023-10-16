3ba6de9954d1a53c017e7d3327fb225a66b1f9b7
zahrahassan14 <118750583+zahrahassan14@users.noreply.github.com>
2023-10-16 21:48:18 +0500

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+﻿let strings = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == '=') {
+            strings = eval(strings);
+            document.querySelector('input').value = strings;
+        }
+        else if (e.target.innerHTML == 'C') {
+            strings = ""
+            document.querySelector('input').value = strings;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            strings = ""
+            document.querySelector('input').value = strings;
+        }
+        else {
+            console.log(e.target)
+            strings = strings + e.target.innerHTML;
+            document.querySelector('input').value = strings;
+        }
+    } )
+})
